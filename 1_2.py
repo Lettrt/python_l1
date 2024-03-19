13519c0ceba2e43910ecc37f27d75188b571e21d
админ <admin@MacBook-Air-admin.local>
2024-03-19 16:15:58 +0600

    mentor commit

---
+++ 1_2.py
@@ -0,0 +1,9 @@
+day=20
+month=11
+year = 2006
+current_year = 2024
+
+
+my_age = 2024 - (year)
+print (my_age)
+
---
+++ homework_self.py
@@ -0,0 +1,30 @@
+a = 5
+b = 10
+
+sum_result = a + b
+div_result = a - b
+sum_result = a * b 
+div_result = a / b
+
+print(f"Результат сложения переменных = {sum_result}")
+print(f" Результат вычитания переменных {div_result}")
+print(f"Резульат умножение переменных{sum_result}")
+print(f"Результат деление переменных{div_result}")
+
+
+age = 17
+future_age = age + 5
+current_age_str = f"My age is {17}"
+future_age_str=f"In 5 years i will be {future_age}"
+
+result_str =f"{current_age_str} {future_age_str}"
+
+print(result_str)
+
+
+total_minutes = 135
+
+hours = total_minutes // 60
+minutes = total_minutes % 60
+
+print(f"{total_minutes} минут это {hours} часов {minutes} минут.")
