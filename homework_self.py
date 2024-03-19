422afea903956882a5f5f003910c41d3a1784235
taalai122 <kiddd3d@gmail.com>
2024-03-19 20:13:05 +0600

    create homework

---
+++ homework_self.py
@@ -0,0 +1,26 @@
+#Задача 1
+
+num1 = 12
+num2 = 6
+
+sum_result = num1 + num2
+sub_result = num1 - num2
+div_result = num1 / num2
+mult_result = num1 * num2
+
+print(f" Sum is {sum_result} \n Subtraction is {sub_result} \n Division is {sub_result} \n multiplication is {mult_result} \n ")
+
+#Задача 2
+
+current_age = 22
+post_age = current_age + 5
+
+print(f"You are {current_age} years old, \n" + f"After 5 years you'll be {post_age} years old ")
+
+#Задача 3
+
+total_mins = 195
+hour = total_mins // 60
+minute = total_mins % 60
+
+print(f"{hour} hours " f"{minute} minutes")
