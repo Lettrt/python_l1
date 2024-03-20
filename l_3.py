b6e2fa319c39849963762f3e948bd52dfaf3191f
Alexey Kosarev <ak555110689@gmail.com>
2024-03-20 20:11:02 +0600

    calculator

---
+++ l_3.py
@@ -0,0 +1,18 @@
+num1 = int(input('Enter 1st number '))
+operator = input('Enter + - * /')
+num2 = int(input('Enter 2nd number '))
+
+if operator == '+':
+    result = num1 + num2
+    print(f'Result is {result}')
+elif operator == '-':
+    result = num1 - num2
+    print(f'Result is {result}')
+elif operator == '*':
+    result = num1 * num2
+    print(f'Result is {result}')
+elif operator == '/':
+    result = num1 / num2
+    print(f'Result is {result}')
+else:
+    print('Неправильный оператор!!!!!')
